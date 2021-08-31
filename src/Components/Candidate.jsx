4da602d5f921d8a61f67dcdce262314789565991
venilson1 <venilson2009@hotmail.com>
2021-08-31 14:09:30 -0300

    fix: resolved bug image an prod

--- src/Components/Candidate.jsx
+++ src/Components/Candidate.jsx
@@ -23,7 +23,7 @@ export default function Candidate({
                 <div key={id} style={{ backgroundColor: "#606368" }} className="m-8 p-4 rounded-lg shadow-md font-medium w-96 ">
                   <div className="flex flex-row p-4">
                     <div className="flex w-2/4 justify-center ">
-                      <img src={`img/${username}.png`} alt="" className={`rounded-full h-24 ${borderElected} border-4`} />
+                      <img src={`img/${name}.png`} alt={name} className={`rounded-full h-24 ${borderElected} border-4`} />
                     </div>
                     <div className="flex col-end-13 w-2/4 items-center justify-center p-2 flex-col">
                       <p className={`font-extrabold text-2xl ${textElected}`}>{percent} %</p>
