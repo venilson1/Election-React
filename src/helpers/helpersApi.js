0cf18a75e78135667d3859299b218bc323944b09
venilson1 <venilson2009@hotmail.com>
2021-08-31 02:37:28 -0300

    feat: create api ideal yo consumed

---
+++ src/helpers/helpersApi.js
@@ -0,0 +1,32 @@
+export default function helpersApi(
+  election = [],
+  allCities = [],
+  allCandidates = []
+) {
+  let obj;
+  election.forEach((el) => {
+    allCities.forEach((city) => {
+      if (el.cityId === city.id) {
+        el.cityName = city.name;
+        el.percent = ((el.votes * 100) / city.presence).toFixed(2);
+      }
+    });
+
+    allCandidates.forEach((candidate) => {
+      if (el.candidateId === candidate.id) {
+        el.candidateName = candidate.name;
+        el.candidateUsername = candidate.username;
+      }
+    });
+
+    obj = allCities.filter(city => {
+      return city.id === el.cityId
+    })
+
+    return obj.forEach(element => {
+      element.candidates = el
+    })
+  });
+
+  return obj;
+}
